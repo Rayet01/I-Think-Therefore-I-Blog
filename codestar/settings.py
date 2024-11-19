08a19e452ee4b85de7810c0b0e207b1064bce812
Sol Rayet <ge9i3fg8@students.codeinstitute.net>
2024-11-19 11:02:05 +0000

    Add summernote package, creat new blog post in admin panel

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kqab1h#2hqvw@em^3c%xi$us_*)3(da&b5p0)8*fz9a&@4d9sn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-rayet01-ithinktherefore-nkpl69u1so1.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
